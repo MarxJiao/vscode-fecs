64672a5a04bffdc681e78f2bf4fe94c487f7f622
MarxJiao <610529111@qq.com>
2016-05-26 22:19:21 +0800

    add config to controll output language

--- src/extension.ts
+++ src/extension.ts
@@ -12,34 +12,44 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "fecs" is now active!');
-
+    
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.fecs', () => {
+        
         // 命令代码
-        let fecsCmd = 'fecs ' + vscode.window.activeTextEditor.document.fileName + ' --reporter=baidu';
-        
-        // 新建output
-        let output = vscode.window.createOutputChannel('fecs output');
-        
-        // 运行命令并将结果输出到output
-        child_process.exec(fecsCmd, null, function (error, stdout, stderr) {
-            vscode.workspace.saveAll();
-            // vscode.window.showInformationMessage(stdout);
-            console.log(stdout);
-            output.append(stdout);
-            output.show(5,true);
-        })
-        
-        
+        let ifEnglish = vscode.workspace.getConfiguration('fecs').get('en');
+        let reporter = ''
+        if (ifEnglish) {
+            reporter = '';
+        }
+        else {
+            reporter = ' --reporter=baidu';
+        }
+        let fecsCmd = 'fecs ' + vscode.window.activeTextEditor.document.fileName + reporter;
+        console.log(ifEnglish);
+        showOutput(fecsCmd);
         // Display a message box to the user
         // vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
 }
-
+function showOutput(cmd:string) {
+    
+    // 新建output
+    let output = vscode.window.createOutputChannel('fecs output');
+    
+    // 运行命令并将结果输出到output
+    child_process.exec(cmd, null, function (error, stdout, stderr) {
+        vscode.workspace.saveAll();
+        // vscode.window.showInformationMessage(stdout);
+        console.log(stdout);
+        output.append(stdout);
+        output.show(5,true);
+    })
+}
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
