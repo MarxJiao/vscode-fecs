1a7d064a60e7b2de3d8f0e0243c0d98a8ab988a4
MarxJiao <610529111@qq.com>
2016-05-27 14:24:08 +0800

    添加配置选项、图片目录修改

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # vscode fecs插件
 fecs extension for vscode
+# feature
+ - 支持中英文切换（配置fecs.en, true: English, false: 中文）
+ - 支持保存时自动使用fecs检查（配置fecs.auto, true: 自动检查, false: 输入命令或快捷键```cmd + shift + r```）
+ 
 # install
 ### 安装 [fecs](http://fecs.baidu.com/)
 ```
@@ -11,9 +15,24 @@ sudo npm install fecs -g
 ext install fecs
 ```
 
-# use 
+# configuration
 
-在打开的Javascript、HTML、CSS等fecs支持的文件下
+按```cmd + ,```打开用户配置，若使用默认配置则不用修改
+
+```
+{
+    // 配置是否使用英文显示，{boolean}
+    // 默认false，使用中文。设置为true，则使用英文
+    "fecs.en": false,
+
+    // 配置是否在保存文件时自动使用fecs，{boolean}
+    // 本着不扰民的思想，默认false，不自动使用。设置为true，则在保存时自动使用fecs
+    "fecs.auto": true
+}
+```
+# use 
+### 1. 命令操作
+在打开的javascript、HTML、CSS等fecs支持的文件下
 
 按 ```cmd + shift + p```
 
@@ -21,7 +40,10 @@ ext install fecs
 ```
 fecs
 ```
+### 2. 快捷键
+```cmd + shift + r```
+
 之后就能看见fecs输出的结果，用output输出比较方便多条信息查看，下面是演示
 
 
-![img](fecs1.gif)
+![img](imges/fecs1.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -13,43 +13,73 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "fecs" is now active!');
     
+    
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.fecs', () => {
         
-        // 命令代码
-        let ifEnglish = vscode.workspace.getConfiguration('fecs').get('en');
-        let reporter = ''
-        if (ifEnglish) {
-            reporter = '';
+        let fileType = vscode.window.activeTextEditor.document.languageId;
+        console.log(fileType);
+        if (
+            fileType === 'javascript' 
+            || fileType === 'html' 
+            || fileType === 'css' 
+            || fileType === 'less'
+        ) {
+            let ifEnglish = vscode.workspace.getConfiguration('fecs').get('en');
+            let reporter = ''
+            if (ifEnglish) {
+                reporter = '';
+            }
+            else {
+                reporter = ' --reporter=baidu';
+            }
+            // if display output in English if 'fecs.en' is true
+            let fecsCmd = 'fecs ' + vscode.window.activeTextEditor.document.fileName + reporter;
+            showOutput(fecsCmd);
         }
         else {
-            reporter = ' --reporter=baidu';
+            deactivate();
+            return false;
         }
-        let fecsCmd = 'fecs ' + vscode.window.activeTextEditor.document.fileName + reporter;
-        console.log(ifEnglish);
-        showOutput(fecsCmd);
+        
+       
         // Display a message box to the user
         // vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
 }
+let output = vscode.window.createOutputChannel('fecs output');
 function showOutput(cmd:string) {
     
     // 新建output
-    let output = vscode.window.createOutputChannel('fecs output');
+    
     
     // 运行命令并将结果输出到output
     child_process.exec(cmd, null, function (error, stdout, stderr) {
         vscode.workspace.saveAll();
         // vscode.window.showInformationMessage(stdout);
         console.log(stdout);
+        output.clear();
         output.append(stdout);
         output.show(5,true);
     })
 }
+
+// automatically use fecs on did save file if 'extension.fecs' is true
+vscode.workspace.onDidSaveTextDocument(() => {
+    let ifAutoFecs = vscode.workspace.getConfiguration('fecs').get('auto');
+    if (ifAutoFecs) {
+        vscode.commands.executeCommand('extension.fecs');
+    }
+    else {
+        return false;
+    }
+})
 // this method is called when your extension is deactivated
 export function deactivate() {
+    output.clear();
+    output.hide();
 }
